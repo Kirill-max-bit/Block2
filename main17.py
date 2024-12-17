4b5af968bc190828034ce71d539aee2eafe35484
Kirill-max-bit <soloncenkokirill64@gmail.com>
2024-12-17 10:54:31 +0000

    сделал задание

--- main17.py
+++ main17.py
@@ -2,13 +2,10 @@ def swap_digits(number):
 
     num_str = str(number)
 
-
     if len(num_str) != 3:
         raise ValueError("Число должно быть трехзначным.")
 
-
     swapped_str = num_str[0] + num_str[2] + num_str[1]
-
 
     swapped_number = int(swapped_str)
 
