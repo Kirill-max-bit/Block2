15cc82d623175a1bc3e6d20ffd1d9d8551612a4d
Kirill-max-bit <soloncenkokirill64@gmail.com>
2024-12-19 10:02:41 +0000

    Сделал задачу

--- main32.py
+++ main32.py
@@ -1,1 +1,17 @@
+target_number = 654
+
+
+last_digit = target_number // 100        
+first_digit = (target_number // 10) % 10  
+second_digit = target_number % 10         
+
+
+for x in range(100, 1000):
+    hundreds = x // 100          
+    tens = (x // 10) % 10       
+    units = x % 10               
+
+    if units == last_digit and (tens * 10 + hundreds) == (first_digit * 10 + second_digit):
+        print(f"Найденное трехзначное число x: {x}")
+        break
 
