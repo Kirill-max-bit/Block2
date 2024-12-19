3ed6dd218ece20a1fb2e07c22b407921651f054b
Kirill-max-bit <soloncenkokirill64@gmail.com>
2024-12-19 08:25:47 +0000

    сделал задачу

--- main29.py
+++ main29.py
@@ -5,11 +5,11 @@ if 10 <= n <= 999 and (n // 10) % 10 != 0:
     tens = n // 10  
     units = n % 10 
 
-    
+
     second_digit = tens
     first_digit = (n // 100) if (n >= 100) else (tens // 10)
     
-    
+   
     x = first_digit * 100 + second_digit * 10 + units
 
     print(f"Найденное трехзначное число x: {x}")
