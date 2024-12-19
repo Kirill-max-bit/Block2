9199ca8793685f8394105d7b0a6dc04886f1d532
Kirill-max-bit <soloncenkokirill64@gmail.com>
2024-12-19 09:59:28 +0000

    Сделал задачу

--- main28.py
+++ main28.py
@@ -1,8 +1,7 @@
-# Ищем число x
-for x in range(100, 1000):  # Перебор всех трехзначных чисел
+for x in range(100, 1000):  
     str_x = str(x)
-    second_digit = str_x[1]  # Вторая цифра
+    second_digit = str_x[1]  
     new_number = int(second_digit + str_x[0] + str_x[2])
-    if new_number == 546:  # Проверяем условие
+    if new_number == 546:  
         print(f"Найдено число x: {x}")
-        break  # Прерываем цикл, так как нужное x найдено
+        break 
